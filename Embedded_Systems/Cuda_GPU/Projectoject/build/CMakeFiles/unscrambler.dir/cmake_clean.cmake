fe5e7f63cc5c027c0c82b071d5fba280cb6fb35b
OwaisAhmed19 <101355018+owaisahmed19@users.noreply.github.com>
2026-02-25 00:17:36 +0100

    Add files via upload

---
+++ Embedded_Systems/Cuda_GPU/Projectoject/build/CMakeFiles/unscrambler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unscrambler.dir/unscrambler.cu.o"
+  "CMakeFiles/unscrambler.dir/cmake_device_link.o"
+  "unscrambler.pdb"
+  "unscrambler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/unscrambler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
